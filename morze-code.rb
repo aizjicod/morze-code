970af35bebdf05a1ce4a722fe33532febb320972
wayungi <fwayungi@gmail.com>
2022-09-22 01:40:43 +0300

    Solution

--- morze-code.rb
+++ morze-code.rb
@@ -1,3 +1,50 @@
-def sum(a, b)
-  puts(a + b)
+def decode_char(code)
+  morse_hash = { ".-" => "A",
+                 "-..." => "B",
+                 "-.-." => "c",
+                 "-.." => "D",
+                 "." => "E",
+                 "..-." => "F",
+                 "--." => "G",
+                 "...." => "H",
+                 ".." => "I",
+                 ".---" => "J",
+                 "-.-" => "K",
+                 ".-.." => "L",
+                 "--" => "M",
+                 "-." => "N",
+                 "---" => "O",
+                 ".--." => "P",
+                 "--.-" => "Q",
+                 ".-." => "R",
+                 "..." => "S",
+                 "-" => "T",
+                 "..-" => "U",
+                 "...-" => "V",
+                 ".--" => "W",
+                 "-..-" => "X",
+                 "-.--" => "Y",
+                 "--.." => "Z" }
+  morse_hash[code]
 end
+
+# puts(decode_char("--.."))
+
+def decode_word(morse)
+  word = ""
+  for code in morse.split(" ")
+    word += decode_char(code)
+  end
+  word
+end
+# puts(decode_word("-... -.- -.."))
+
+def decode(statement)
+  sentence = ""
+  for word in statement.split("   ")
+    sentence += " " + decode_word(word)
+  end
+  sentence
+end
+
+puts(decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."))
